4009a567e60508d93a8713155b0dc40f1b8aa800
Felipe-Sousa-Cardoso <expraka@gmail.com>
2025-06-11 14:11:36 -0300

    Criação do mapa multiplayer

---
+++ Projeto-Imperial/Assets/Scripts/Servidor/Mapa/CelulaNoMapa.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CelulaNoMapa : MonoBehaviour
+{
+    Vector2 _posição;
+
+    public Vector2 Posição { get => _posição; set => _posição = value; }
+
+    void Start()
+    {
+        gameObject.name = Posição.x + "/" + Posição.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        print(Posição);
+    }
+}
--- Projeto-Imperial/Assets/Scripts/Servidor/Mapa/CriaçãoDeMapa.cs
+++ Projeto-Imperial/Assets/Scripts/Servidor/Mapa/CriaçãoDeMapa.cs
@@ -1,15 +1,18 @@
+using System;
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
 public class CriaçãoDeMapa : NetworkBehaviour
 {
+    NetworkList<InformacoesDaCelula> lista;
     [SerializeField] GameObject CadaCélula;
     [SerializeField] int _altura;
     [SerializeField] int _largura;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        lista = new NetworkList<InformacoesDaCelula>();
     }
     public override void OnNetworkSpawn()
     {
@@ -17,6 +20,10 @@ public class CriaçãoDeMapa : NetworkBehaviour
         {
             GerarMapa();
         }
+        if (IsClient)
+        {           
+            IntanciarMapa();           
+        }      
     }
     void GerarMapa()
     {
@@ -24,17 +31,34 @@ public class CriaçãoDeMapa : NetworkBehaviour
         {
             for (int indexAltura = 0; indexAltura < _altura; indexAltura++)
             {
-                GameObject obj = Instantiate(CadaCélula, new Vector3(indexLargura - _largura / 2, indexAltura - _altura / 2), Quaternion.identity, transform);
-                obj.name = $"Tile {indexLargura},{indexAltura}";
-
-                NetworkObject networkObject = obj.GetComponent<NetworkObject>();
-                if (networkObject != null)
-                {
-                    networkObject.Spawn();
-                }
-
+                Vector3 pos = new Vector3(indexLargura - _largura / 2, indexAltura - _altura / 2);
+                lista.Add(
+                    new InformacoesDaCelula
+                    {
+                        posi = pos
+                    }
+                    );
             }
         }
     }
-    
+    private void IntanciarMapa()
+    {
+        foreach (InformacoesDaCelula celula in lista)
+        {
+            GameObject obj = Instantiate(CadaCélula, celula.posi, Quaternion.identity);
+            obj.GetComponent<CelulaNoMapa>().Posição = celula.posi;
+        }
+    }
+}
+public struct InformacoesDaCelula : INetworkSerializable, IEquatable<InformacoesDaCelula>
+{
+    public Vector2 posi;
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref posi);
+    }
+    public bool Equals(InformacoesDaCelula other)
+    {
+        return posi == other.posi;
+    }
 }
